b56bc76ce3745d09fcffd96e294f13ae0cb7b622
kai <kai@turbot.com>
2021-06-14 17:20:02 +0100

    add SetULimit and GetULimit. Closes #11

---
+++ files/ulimit.go
@@ -0,0 +1,18 @@
+package files
+
+import (
+	"syscall"
+)
+
+func SetULimit(ulimit uint64) error {
+	var rLimit syscall.Rlimit
+	rLimit.Max = ulimit
+	rLimit.Cur = ulimit
+	return syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+}
+
+func GetULimit() (syscall.Rlimit, error) {
+	var rLimit syscall.Rlimit
+	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit)
+	return rLimit, err
+}
