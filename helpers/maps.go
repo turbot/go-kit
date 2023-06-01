575abaf197899e26a773963326404e59f0ce489c
Binaek Sarkar <binaek@turbot.com>
2023-06-01 19:31:27 +0530

    clone maps

--- helpers/maps.go
+++ helpers/maps.go
@@ -1,19 +1,21 @@
 package helpers
 
 import (
-	"golang.org/x/exp/maps"
 	"sort"
+
+	"golang.org/x/exp/maps"
 )
 
 // MergeMaps merges 'new' onto 'old'.
 // Values existing in old already have precedence
 // Any value existing in new but not old is added to old
+// returns a copy of the original map
 func MergeMaps[M ~map[K]V, K comparable, V any](old, new M) M {
 	if old == nil {
-		return new
+		return maps.Clone(new)
 	}
 	if new == nil {
-		return old
+		return maps.Clone(old)
 	}
 	res := maps.Clone(old)
 	for k, v := range new {
