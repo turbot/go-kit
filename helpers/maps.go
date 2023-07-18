48cbd83fe73e1e069e7e3a78acf40092f1693eea
kai <kai@turbot.com>
2023-07-18 13:35:43 +0100

    Add FilterMap. Closes #59

--- helpers/maps.go
+++ helpers/maps.go
@@ -27,16 +27,29 @@ func MergeMaps[M ~map[K]V, K comparable, V any](old, new M) M {
 	return res
 }
 
+// SortedMapKeys returnes the sorted keys of the map `m`
 func SortedMapKeys[V any](m map[string]V) []string {
 	var keys = maps.Keys(m)
 	sort.Strings(keys)
 	return keys
 }
 
+// SliceToLookup converts a slice into a lookup
 func SliceToLookup[K comparable](src []K) map[K]struct{} {
 	var result = make(map[K]struct{}, len(src))
 	for _, k := range src {
 		result[k] = struct{}{}
+	}
+	return result
+}
+
+// FilterMap builds a map based on `src`, but using only keys specified in `keys`
+func FilterMap[K comparable, V any](src map[K]V, keys []K) map[K]V {
+	var result = make(map[K]V, len(keys))
+	for _, k := range keys {
+		if v, ok := src[k]; ok {
+			result[k] = v
+		}
 	}
 	return result
 }
