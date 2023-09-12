4403c82321c0c257d55e2bad95d28090d63e3904
Binaek Sarkar <binaek@turbot.com>
2023-09-12 18:56:51 +0530

    refactor

--- logging/rotatinglogwriter.go
+++ logging/rotatinglogwriter.go
@@ -27,6 +27,35 @@ func NewRotatingLogWriter(directory string, prefix string) *RotatingLogWriter {
 		prefix:    prefix,
 	}
 }
+func (dwr *RotatingLogWriter) rotateLogTarget(targetPath string) (err error) {
+	dwr.rotateLock.Lock()
+	defer dwr.rotateLock.Unlock()
+
+	// update to the current path
+	dwr.currentPath = targetPath
+
+	// check if the file actually doesn't exist
+	// another thread may have created it while we were waiting for the lock
+	if !files.FileExists(targetPath) {
+		// we need to flush the current one
+		// try to cast it to a Closer (if this is nil, isCloseable will be false)
+		closeableWriter, isCloseable := dwr.currentWriter.(io.Closer)
+		if isCloseable {
+			closeableWriter.Close()
+		}
+	}
+
+	// we could be in here because the file exists,
+	// but we are starting up for the first time
+	if dwr.currentWriter == nil {
+		// create a new one
+		dwr.currentWriter, err = os.OpenFile(dwr.currentPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			return fmt.Errorf("failed to open steampipe log file: %s", err.Error())
+		}
+	}
+	return nil
+}
 
 func (dwr *RotatingLogWriter) Write(p []byte) (n int, err error) {
 	pathShouldBe := filepath.Join(dwr.directory, fmt.Sprintf("%s-%s.log", dwr.prefix, time.Now().Format(time.DateOnly)))
@@ -36,31 +65,8 @@ func (dwr *RotatingLogWriter) Write(p []byte) (n int, err error) {
 	// the code inside the IF block will probably execute once in 24 hours at most
 	// for an instance, but the code outside is used by every log line
 	if dwr.currentPath != pathShouldBe {
-		dwr.rotateLock.Lock()
-		defer dwr.rotateLock.Unlock()
-
-		// update to the current path
-		dwr.currentPath = pathShouldBe
-
-		// check if the file actually doesn't exist
-		// another thread may have created it while we were waiting for the lock
-		if !files.FileExists(pathShouldBe) {
-			// we need to flush the current one
-			// try to cast it to a Closer (if this is nil, isCloseable will be false)
-			closeableWriter, isCloseable := dwr.currentWriter.(io.Closer)
-			if isCloseable {
-				closeableWriter.Close()
-			}
-		}
-
-		// we could be in here because the file exists,
-		// but we are starting up for the first time
-		if dwr.currentWriter == nil {
-			// create a new one
-			dwr.currentWriter, err = os.OpenFile(dwr.currentPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-			if err != nil {
-				return 0, fmt.Errorf("failed to open steampipe log file: %s", err.Error())
-			}
+		if err := dwr.rotateLogTarget(pathShouldBe); err != nil {
+			return 0, err
 		}
 	}
 
