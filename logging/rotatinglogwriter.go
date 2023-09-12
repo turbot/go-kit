dea16db81714661817ed7e7130f445a02b340d39
Binaek Sarkar <binaek@turbot.com>
2023-09-12 19:04:17 +0530

    tweaks

--- logging/rotatinglogwriter.go
+++ logging/rotatinglogwriter.go
@@ -27,29 +27,32 @@ func NewRotatingLogWriter(directory string, prefix string) *RotatingLogWriter {
 		prefix:    prefix,
 	}
 }
-func (dwr *RotatingLogWriter) rotateLogTarget(targetPath string) (err error) {
-	dwr.rotateLock.Lock()
-	defer dwr.rotateLock.Unlock()
+func (w *RotatingLogWriter) rotateLogTarget(targetPath string) (err error) {
+	w.rotateLock.Lock()
+	defer w.rotateLock.Unlock()
 
 	// update to the current path
-	dwr.currentPath = targetPath
+	w.currentPath = targetPath
 
 	// check if the file actually doesn't exist
-	// another thread may have created it while we were waiting for the lock
-	if !files.FileExists(targetPath) {
-		// we need to flush the current one
-		// try to cast it to a Closer (if this is nil, isCloseable will be false)
-		closeableWriter, isCloseable := dwr.currentWriter.(io.Closer)
-		if isCloseable {
-			closeableWriter.Close()
-		}
+	if files.FileExists(targetPath) {
+		// nothing to do here
+		// another thread may have created it while we were waiting for the lock
+		return nil
+	}
+
+	// we need to flush the current one
+	// try to cast it to a Closer (if this is nil, isCloseable will be false)
+	closeableWriter, isCloseable := w.currentWriter.(io.Closer)
+	if isCloseable {
+		closeableWriter.Close()
 	}
 
 	// we could be in here because the file exists,
 	// but we are starting up for the first time
-	if dwr.currentWriter == nil {
+	if w.currentWriter == nil {
 		// create a new one
-		dwr.currentWriter, err = os.OpenFile(dwr.currentPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		w.currentWriter, err = os.OpenFile(w.currentPath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 		if err != nil {
 			return fmt.Errorf("failed to open steampipe log file: %s", err.Error())
 		}
@@ -57,18 +60,18 @@ func (dwr *RotatingLogWriter) rotateLogTarget(targetPath string) (err error) {
 	return nil
 }
 
-func (dwr *RotatingLogWriter) Write(p []byte) (n int, err error) {
-	pathShouldBe := filepath.Join(dwr.directory, fmt.Sprintf("%s-%s.log", dwr.prefix, time.Now().Format(time.DateOnly)))
+func (w *RotatingLogWriter) Write(p []byte) (n int, err error) {
+	expectedPath := filepath.Join(w.directory, fmt.Sprintf("%s-%s.log", w.prefix, time.Now().Format(time.DateOnly)))
 
 	// the code outside of this IF block should be simple and blazing fast
 	//
 	// the code inside the IF block will probably execute once in 24 hours at most
 	// for an instance, but the code outside is used by every log line
-	if dwr.currentPath != pathShouldBe {
-		if err := dwr.rotateLogTarget(pathShouldBe); err != nil {
+	if w.currentPath != expectedPath {
+		if err := w.rotateLogTarget(expectedPath); err != nil {
 			return 0, err
 		}
 	}
 
-	return dwr.currentWriter.Write(p)
+	return w.currentWriter.Write(p)
 }
