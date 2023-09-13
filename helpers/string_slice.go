543fcb23cdcd3648ab991e5883184c9240565491
Binaek Sarkar <binaek@turbot.com>
2023-09-13 23:23:36 +0530

    remove StringSliceDiff

--- helpers/string_slice.go
+++ helpers/string_slice.go
@@ -1,18 +1,5 @@
 package helpers
 
-import (
-	"sort"
-
-	"github.com/google/go-cmp/cmp"
-	"github.com/google/go-cmp/cmp/cmpopts"
-)
-
-func StringSliceEqualIgnoreOrder(a, b []string) bool {
-	less := func(a, b string) bool { return a < b }
-	return cmp.Diff(a, b, cmpopts.SortSlices(less)) == ""
-
-}
-
 // StringSliceContains returns whether the string slice contains the given string
 func StringSliceContains(slice []string, val string) bool {
 	for _, item := range slice {
@@ -21,35 +8,4 @@ func StringSliceContains(slice []string, val string) bool {
 		}
 	}
 	return false
-}
-
-// StringSliceDiff returns values which only exists in the fist string slice
-func StringSliceDiff(slice1, slice2 []string) (onlyInSlice1 []string) {
-	onlyInSlice1 = []string{}
-	if len(slice1) == 0 {
-		return
-	}
-	if len(slice2) == 0 {
-		onlyInSlice1 = slice1
-		return
-	}
-
-	sort.Strings(slice1)
-	sort.Strings(slice2)
-
-	idx := 0
-	for _, item := range slice1 {
-		// ignore empty
-		if item == "" {
-			continue
-		}
-
-		for slice2[idx] < item && idx+1 < len(slice2) {
-			idx++
-		}
-		if slice2[idx] != item {
-			onlyInSlice1 = append(onlyInSlice1, item)
-		}
-	}
-	return
 }
