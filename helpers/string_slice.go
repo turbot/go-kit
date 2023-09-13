c123c59523689954d6485a3d8da7fbc64dbfaa78
Binaek Sarkar <binaek@turbot.com>
2023-09-13 23:24:04 +0530

    Remove StringSliceContains

--- helpers/string_slice.go
+++ helpers/string_slice.go
@@ -1,11 +1,1 @@
 package helpers
-
-// StringSliceContains returns whether the string slice contains the given string
-func StringSliceContains(slice []string, val string) bool {
-	for _, item := range slice {
-		if item == val {
-			return true
-		}
-	}
-	return false
-}
