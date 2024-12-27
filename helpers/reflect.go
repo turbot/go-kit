9a690e4247d50605e1b5f6099d552160ca7f6e51
rinor <rinorhoxha@gmail.com>
2024-12-27 10:20:28 +0000

    fix: update `IsNil` function to exclude Array type check
    - Remove Array type from `IsNil` function checks
    - Fixes `call of reflect.Value.IsNil on array Value` - https://github.com/turbot/steampipe-plugin-sdk/issues/320

--- helpers/reflect.go
+++ helpers/reflect.go
@@ -47,7 +47,7 @@ func IsNil(i interface{}) bool {
 		return true
 	}
 	switch reflect.TypeOf(i).Kind() {
-	case reflect.Ptr, reflect.Map, reflect.Array, reflect.Chan, reflect.Slice:
+	case reflect.Ptr, reflect.Map, reflect.Chan, reflect.Slice:
 		return reflect.ValueOf(i).IsNil()
 	}
 	return false
